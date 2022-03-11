7a4ca63a23490c49b176934bd6914c364816a07d
IlarioCOCCO <ilario.cocco@gmail.com>
2022-03-11 22:04:19 +0100

    recap js

--- js/scripts.js
+++ js/scripts.js
@@ -109,103 +109,101 @@ const boxIcon = [
         category: "animal"
     },
 ];
-// -------------------------------------------------------------------------
+
+
+//----------------------------------------FOR EACH--------------------------------------------------//
 // PROGRAMMA PRINCIPALE
 // contenitore di Icone
-const contboxIcon = document.getElementById("boxicon");
+const contboxIcon = document.getElementById("boxicon"); // lo uso anche per la funzione stampa
+
 // ciclo su tutte le Icone ed inserisco il markup HTML da inserire:
-    boxIcon.forEach((element) => { 
-    // console.log(element); //debuging ------!!!!!!!!!!!!! verifica in console
-    const { name, family, prefix } = element; // attraverso la destrutturazione tiro fuori le proprietà dell obj sottoforma di variabili
-    contboxIcon.innerHTML += `<div class="squareAnimal"><i class="${family} ${prefix}${name}"></i><div class="nomi-Icone">${name}</div></div>`;
-    }
-);
-// --------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------forEach copiato nella function stampaIcone--------------------------
+//     boxIcon.forEach((element) => { 
+   
+//     const { name, family, prefix} = element; // attraverso la destrutturazione tiro fuori le proprietà dell obj sottoforma di variabili
+//     contboxIcon.innerHTML += `<div class="squareAnimal"><i class="${family} ${prefix}${name}"></i><div class="nomi-Icone">${name}</div></div>`;
+//     }
+// );
+// ---------------FOR EACH-----------------------forEach copiato nella function stampaIcone----------------------------------------------------------------------
 
-
-
-
-
+// N.B: ----------------Creazione di una funzione a scopo didatico da riutilizzare, che Stampi, accettando 2 parametri in ingresso:
 const stampaIcone = (array, container) => {
 
-    container.innerHTML = "";
+    container.innerHTML = ""; // vuoto il container
 
     array.forEach((element) => {
-        // console.log(element); //debuging ------!!!!!!!!!!!!! verifica in console
+        
         const { name, family, prefix, color } = element; // attraverso la destrutturazione tiro fuori le proprietà dell obj sottoforma di variabili
         container.innerHTML += `<div class="squareAnimal"><i class="${family} ${prefix}${name}" style="color: ${color}"></i><div class="nomi-Icone">${name}</div> </div>`;
         }
     );
 }
+// SOSTITUISCO boxIcon = array
+// SOSTITUISCO contboxIcon = container
+//--------------------------------------------------------------------------------------------------//
 
 
 
 
 
-// coloriamo le Icone ***
+
+
+//-----------------------------------MAP--------------------------------------------------------------//
+//-----------------------------------------coloriamo le Icone ***
+// -----------const colors = -----------info che potrebbe arrivarci dall'esterno
 const colors = {
     food: "violet",
     animal: "green",
     beverage: "orange"
 };
 
-
-
-// coloriamo le Icone 
-const colorIcon = boxIcon.map(
-    (element) => {
-        // console.log(element)
-        // element.color = "verifica";
-        // console.log(element.category);
-        element.color = colors[element.category]; //***se voglio accedere alle proprietà devo usar le parentesi quadrate perchè il valore è all inrerno di una variabile
+const colorIcon = boxIcon.map((element) => { element.color = colors[element.category]; //***se voglio accedere alle proprietà devo usar le parentesi quadrate perchè il valore è all inrerno di una variabile
         return {
-            // name: element.name,       //map
-            // family: element.family,
-            // prefix: element.prefix,
-            // category: element.category,
-            ...element,                //spread opertor
-            color: colors[element.category] //creazione di un nuovo oggetto
+            ...element, //spread opertor creazione di un nuovo oggetto
+            color: colors[element.category] // creazione di un nuovo oggetto per non andare a sovrascrivere
         };
     }
 );
 // console.log(colorIcon);
-
-
 stampaIcone(colorIcon, contboxIcon);
+//-----------------------------------------coloriamo le Icone ***
+//--------------------------------------------------------------------------------------------------//
 
 
+
+
+
+
+
+//-------------------------------------FOR EACH------------------------------------------------------//
 // creiamo una select con tipologia icone per poi filtrare nuovamente le Icone
-//creo una option per singole categorie
-
-
-
-const iconeCategorie = [];
+// creo una option per singole categorie
+// estrapolo le categorie dalle icone , sono 3!
+const iconeCategorie = []; //----------> NUOVO ARRAY PER SALVARE LE CATEGORIE
 boxIcon.forEach((element) => {
         if (iconeCategorie.includes(element.category) == false ) {
             iconeCategorie.push(element.category);
         }
     }
 );
-
-// console.log(iconeCategorie);
-
-const selezioneCategorie = document.getElementById("tipologia")
-// console.log(selezioneCategorie);  //debug lettura prova visualizzazione select
-
-
+const selezioneCategorie = document.getElementById("categoria")
 
 iconeCategorie.forEach((element) => {
         selezioneCategorie.innerHTML += `<option value="${element}">${element}</option>`;
     }
 );
+//--------------------------------------------------------------------------------------------------//
 
 
 
-// azione su una select:
+
+
+
+//---------------------------------FILTER ED EVENTO CHANGE------------------------------------------//
+// azione su una select Tramite evento Change:
 selezioneCategorie.addEventListener("change",
     function() {
-        //recupero il valore della sect
-
+        //recupero il valore della select
         const filtroIcone = colorIcon.filter((element) => {
                 if (selezioneCategorie.value == "") {
                     return true; 
@@ -220,7 +218,7 @@ selezioneCategorie.addEventListener("change",
         stampaIcone(filtroIcone, contboxIcon);
     }
 );
-
+//---------------------------------------------------------------------------------------------------//
 
 
 
